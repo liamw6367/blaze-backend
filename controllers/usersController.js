30e1f3bf54eb337b2cb1829f6c6d31246f56a679
sargr86 <sargr86@gmail.com>
2021-10-12 16:14:05 +0400

    Roles fix

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,8 +7,9 @@ exports.getRoles = async (req, res) => {
     res.json(roles);
 };
 
-exports.getUsersByRole = async(req,res) =>{
+exports.getUsersByRole = async (req, res) => {
     let {role_id} = req.query;
-    let roles = await Users.findAll({where: {role_id}, attributes: ['first_name','last_name','email','phone']});
-    res.json(roles);
+    let where = role_id ? {role_id} : {};
+    let users = await Users.findAll({where, attributes: ['first_name', 'last_name', 'email', 'phone']});
+    res.json(users);
 };

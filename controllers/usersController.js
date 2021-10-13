ab51927b8ee9b1f73605d1f2135df1d1a690688d
sargr86 <sargr86@gmail.com>
2021-10-13 11:38:18 +0400

    User profile update

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,7 @@
 const db = require('../models');
 const Users = db.users;
 const UserRoles = db.user_roles;
+const m = require('../config/multer');
 
 exports.getRoles = async (req, res) => {
     let roles = await UserRoles.findAll({});
@@ -12,4 +13,19 @@ exports.getUsersByRole = async (req, res) => {
     let where = role_id ? {role_id} : {};
     let users = await Users.findAll({where, attributes: ['first_name', 'last_name', 'email', 'phone']});
     res.json(users);
+};
+
+exports.updateProfile = async (req, res) => {
+    const {id, ...data} = req.body;
+
+    m.uploadAvatar(req, res, async (err) => {
+
+        let newPassword = data.password;
+        // data.password = bcrypt.hashSync(newPassword, 10);
+        await Users.update(data, {where: {id: id}});
+
+        res.json('OK')
+        // await this.changeJwt({id: id, ...data}, res);
+
+    });
 };
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
+const m = require('../config/multer');
 
 
 router.get('/get-roles', usersController.getRoles);
 router.get('/get-users-by-role', usersController.getUsersByRole);
+router.put('/update-profile', m.uploadAvatar, usersController.updateProfile);
 
 
 module.exports = router;

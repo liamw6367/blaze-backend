de6cc3d4d5d86ad2bb220563ff463817fc638499
sargr86 <sargr86@gmail.com>
2021-10-15 16:26:33 +0400

    Prod build

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
 const m = require('../config/multer');
+const validateUser = require('../validators/validateUser');
 
 
 router.get('/get-roles', usersController.getRoles);
 router.get('/get-users-by-role', usersController.getUsersByRole);
-router.put('/update-profile', m.uploadAvatar, usersController.updateProfile);
+router.put('/update-profile', validateUser.rules, m.uploadAvatar, usersController.updateProfile);
 router.put('/update-driver-details', usersController.updateDriverDetails);
 
 

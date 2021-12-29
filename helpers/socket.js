0241468784e44ac47f8b7b44e15c1671149824e1
liamw6367@gmail.com <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>
2021-12-29 17:11:28 +0400

    Support operator role

--- helpers/socket.js
+++ helpers/socket.js
@@ -10,7 +10,7 @@ const DEFAULT_EXPIRATION_TIME = 3600;
 
 let users = [];
 
-getMessagesFromRedis = async (query = 'chat_idx *') => {
+getMessagesFromRedis = async (query = '*') => {
     // let messages = [];
     // let messagesFromRedis = await redisClient.lRange('messages', 0, -1);
     // messagesFromRedis.map(message => {

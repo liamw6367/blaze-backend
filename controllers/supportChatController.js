3dd1e921f3a8c88a1095fb31258714880d9d96d6
Grigor Sargsian <sargr86@gmail.com>
2022-01-18 15:07:34 +0400

    Mongoose fix

--- controllers/supportChatController.js
+++ controllers/supportChatController.js
@@ -3,9 +3,8 @@ const Users = db.users;
 const SupportChatMessages = db.support_chat_messages;
 
 
-exports.getUserMessages = async (req, res) => {
+exports.getMessages = async (req, res) => {
     let messages = await SupportChatMessages.find({
-        connection_id: {"$in": directConnectionIds}
     }).sort({'created_at': 1});
 };
 
